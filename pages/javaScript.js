7d48a94f3699a1c61e476c66487ce405182a57ea
Dynojezz <tatjana.jessen@studmail.w-hs.de>
2019-03-16 01:10:33 +0100

    update über uns

--- pages/javaScript.js
+++ pages/javaScript.js
@@ -1,18 +1,18 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function myFunction2() {
+  document.getElementById("myDropdown_school").classList.toggle("show_school");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+  if (!event.target.matches('.dropbtn_school')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content_school");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('show_school')) {
+        openDropdown.classList.remove('show_school');
       }
     }
   }
